675c4da6e106952bbb8f745a50b72511115ff95f
Tu Le Thanh <lethanhtu1551998@gmail.com>
2021-04-15 16:06:17 +0700

    fix linting errors

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className='App'>
-      <h1>GET THE DEAL APPLICATION</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <title>IO saver</title>
+      </header>
+      <h1>GET THE DEAL APPLICATION</h1>
+    </div>
+  );
+}
+
+export default App;
